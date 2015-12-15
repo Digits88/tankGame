c540c506b1d0de0d5d7ed4440e8b0e0d378bc4a6
yasirunilan <nilanyasiru@gmail.com>
2015-12-15 18:35:03 +0530

    UI creation started

--- TankGame/TankGame/TankGame/Program.cs
+++ TankGame/TankGame/TankGame/Program.cs
@@ -14,20 +14,20 @@ namespace TankGame
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        //static void Main(string[] args)
-        //{
-        //    using (Game1 game = new Game1())
-        //    {
-        //        game.Run();
-        //    }
-            
-
-
-
-        //}
-
-
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
             }
+
+
+
+
+            //}
+
+
+        }
 #endif
 }
 
